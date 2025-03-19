52cb78027b599b32611eb0e56025bb479a6f7728
zotov <zotov.bogdan2019@yandex.ru>
2025-03-19 12:15:21 +0300

    Init

---
+++ efs/src/main/java/ru/noxly/efs/EfsApplication.java
@@ -0,0 +1,13 @@
+package ru.noxly.efs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfsApplication.class, args);
+    }
+
+}
---
+++ efs/src/test/java/ru/noxly/efs/EfsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.noxly.efs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
